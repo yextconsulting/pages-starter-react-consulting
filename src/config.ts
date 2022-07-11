a9074639dd9ccde8b50e1077a679e5a6af0390e0
Tristan Timblin <ttimblin@yext.com>
2022-07-11 10:52:47 -0400

    chore(Layout): remove default config value
    J=CR-2409

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,7 @@
 import { YextProviderContextType } from '@yext/sites-react-components';
 
 const config: YextProviderContextType = {
-  components: {
-    Link: {
-      target: '_blank',
-    },
-  },
+  components: {},
 };
 
 export default config;
