2087522eacd3bc9ab90921c6fb6aa352da547fda
Andy Luu <andyluu324@gmail.com>
2022-07-19 15:03:58 -0400

    feat(footer): implements Footer Section
    J=CR-2654
    
    TEST=manual

---
+++ src/components/cards/DirectoryCard.tsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import { HoursStatus, Link } from "@yext/sites-react-components";
+import { CardProps } from "src/models/cardComponent";
+
+export default function DirectoryCard(props: CardProps): JSX.Element {
+  const { content, relativePrefixToRoot } = props;
+
+  return (
+    <div className="Directorycard drop-shadow bg-white px-6 py-8 border">
+      <h3 className="mb-4">
+        {content.slug ? (
+          <Link href={relativePrefixToRoot + content.slug}>
+            {content.name}
+          </Link>
+        ) : (
+          content.name
+        )}
+      </h3>
+      
+      {content.hours && (
+        <div className="mb-4">
+          <HoursStatus hours={content.hours} />
+        </div>
+      )}
+
+      {/* TODO(cblair): use address component when we figure out ExtendedAddress typing error */}
+      <div className="mb-4">
+        <div>
+          {content.address.line1} <span>{content.address.city}</span>,{" "}
+          <span>{content.address.region}</span> {content.address.postalCode}
+        </div>
+      </div>
+    </div>
+  )
+}
