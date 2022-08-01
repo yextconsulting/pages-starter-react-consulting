eb76c9a5cbe7bb916f40961f9b7aca8604364e58
Andy Luu <andyluu324@gmail.com>
2022-08-01 14:17:07 -0400

    feat(location): completed the basics for the Testimonials
    Still need to add tailwind css instead of reg css
    
    J=CR-2655
    TEST=manual

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,7 +2,7 @@
 import { Link } from "@yext/sites-react-components";
 import { CTA } from "@yext/types";
 import React from "react";
-import "src/styles/Reviews.css";
+import "src/styles/Testimonials.css";
 
 interface ReviewsProps {
 
@@ -14,114 +14,114 @@ function currentDate() {
   return months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
 }
 
-const Reviews = (props: ReviewsProps) => {
+const Testimonials = (props: ReviewsProps) => {
 
   return (
-    <div className="Reviews">
-      <div className="Reviews-container container">
-        <div className="Reviews-title">
-          Reviews
+    <div className="Testimonials">
+      <div className="Testimonials-container container">
+        <div className="Testimonials-title">
+          Testimonials
         </div>
-        <div className="Reviews-review">
-          <div className="Reviews-reviewContainer">
-            <div className="Reviews-reviewText">
+        <div className="Testimonials-review">
+          <div className="Testimonials-reviewContainer">
+            <div className="Testimonials-reviewText">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className="Reviews-qnaContainer">
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+            <div className="Testimonials-qnaContainer">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   Are you an existing client? (Y/N)
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   Yes.
                 </div>
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   Do you have a conflict of interest with the advisor? (Y/N)
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   No.
                 </div>
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   Have you received any compensation for leaving the review? (Y/N)
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   No.
                 </div>
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   If Yes, provide a description of the material terms of compensation provided.
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </div>
               </div>
             </div>
           </div>
-          <div className="Reviews-reviewerContainer">
-            <div className="Reviews-reviewerName">
+          <div className="Testimonials-reviewerContainer">
+            <div className="Testimonials-reviewerName">
               Name
             </div>
-            <div className="Reviews-reviewDate">
+            <div className="Testimonials-reviewDate">
               {currentDate()}
             </div>
-            <div className="Reviews-verifiedClient">
+            <div className="Testimonials-verifiedClient">
               verified client
             </div>
           </div>
         </div>
-        <div className="Reviews-review">
-          <div className="Reviews-reviewContainer">
-            <div className="Reviews-reviewText">
+        <div className="Testimonials-review">
+          <div className="Testimonials-reviewContainer">
+            <div className="Testimonials-reviewText">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className="Reviews-qnaContainer">
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+            <div className="Testimonials-qnaContainer">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   Are you an existing client? (Y/N)
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   Yes.
                 </div>
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   Do you have a conflict of interest with the advisor? (Y/N)
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   No.
                 </div>
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   Have you received any compensation for leaving the review? (Y/N)
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   No.
                 </div>
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
+              <div className="Testimonials-qna">
+                <div className="Testimonials-qnaQuestion">
                   If Yes, provide a description of the material terms of compensation provided.
                 </div>
-                <div className="Reviews-qnaAnswer">
+                <div className="Testimonials-qnaAnswer">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </div>
               </div>
             </div>
           </div>
-          <div className="Reviews-reviewerContainer">
-            <div className="Reviews-reviewerName">
+          <div className="Testimonials-reviewerContainer">
+            <div className="Testimonials-reviewerName">
               Name
             </div>
-            <div className="Reviews-reviewDate">
+            <div className="Testimonials-reviewDate">
               {currentDate()}
             </div>
-            <div className="Reviews-verifiedClient">
+            <div className="Testimonials-verifiedClient">
               verified client
             </div>
           </div>
@@ -131,4 +131,4 @@ const Reviews = (props: ReviewsProps) => {
   );
 };
 
-export default Reviews;
+export default Testimonials;
