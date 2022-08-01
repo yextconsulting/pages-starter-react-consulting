d03a18a98610b58dabaaf26185100bac3cd3878d
Andy Luu <andyluu324@gmail.com>
2022-08-01 17:21:35 -0400

    feat(location): added recent reviews
    did not add stars or pagnation yet
    
    J=CR-2655
    TEST=manual

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -19,110 +19,44 @@ const Reviews = (props: ReviewsProps) => {
   return (
     <div className="Reviews">
       <div className="Reviews-container container">
-        <div className="Reviews-title">
-          Reviews
+        <div className="Reviews-titleAndRating">
+            <div className="Reviews-title">
+              Recent Reviews
+            </div>
+            {/* Ask Ben how to implement this */}
+            <div className="Reviews-overallRating">
+              ???
+            </div>
         </div>
         <div className="Reviews-review">
-          <div className="Reviews-reviewContainer">
-            <div className="Reviews-reviewText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <div className="Reviews-qnaContainer">
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  Are you an existing client? (Y/N)
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  Yes.
-                </div>
-              </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  Do you have a conflict of interest with the advisor? (Y/N)
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  No.
-                </div>
-              </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  Have you received any compensation for leaving the review? (Y/N)
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  No.
-                </div>
-              </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  If Yes, provide a description of the material terms of compensation provided.
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="Reviews-reviewerContainer">
+          <div className="Reviews-reviewerNameAndDate">
             <div className="Reviews-reviewerName">
-              Name
+                Name
             </div>
             <div className="Reviews-reviewDate">
-              {currentDate()}
-            </div>
-            <div className="Reviews-verifiedClient">
-              verified client
+                {currentDate()}
             </div>
           </div>
-        </div>
-        <div className="Reviews-review">
-          <div className="Reviews-reviewContainer">
+          <div className="Reviews-ratingAndResponse">
+            {/* Ask Ben how to implement this */}
+            <div className="Reviews-rating">
+              ???
+            </div>
             <div className="Reviews-reviewText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque. Ut porta viverra est, ut dignissim elit elementum ut.
             </div>
-            <div className="Reviews-qnaContainer">
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  Are you an existing client? (Y/N)
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  Yes.
-                </div>
+
+            {/* Ask Ben how to make this not always appear */}
+            <div className="Reviews-businessResponse">
+              <div className="Reviews-businessName">
+                  Business Name
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  Do you have a conflict of interest with the advisor? (Y/N)
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  No.
-                </div>
+              <div className="Reviews-responseDate">
+                {currentDate()}
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  Have you received any compensation for leaving the review? (Y/N)
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  No.
-                </div>
+              <div className="Reviews-responseText">
+                Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor.
               </div>
-              <div className="Reviews-qna">
-                <div className="Reviews-qnaQuestion">
-                  If Yes, provide a description of the material terms of compensation provided.
-                </div>
-                <div className="Reviews-qnaAnswer">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="Reviews-reviewerContainer">
-            <div className="Reviews-reviewerName">
-              Name
-            </div>
-            <div className="Reviews-reviewDate">
-              {currentDate()}
-            </div>
-            <div className="Reviews-verifiedClient">
-              verified client
             </div>
           </div>
         </div>
