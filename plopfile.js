708732affb06a025de6233491bfbfa8530058d2f
Tristan Timblin <ttimblin@yext.com>
2022-08-03 15:30:14 -0400

    call plop.load()

---
+++ plopfile.js
@@ -0,0 +1,9 @@
+const config = (plop) => {
+  plop.load("./plopfiles/component.js");
+  plop.load("@yext/sites-react-starters");
+
+  // console.log(plop);
+  // console.log(plop.getGeneratorList());
+};
+
+export default config;
