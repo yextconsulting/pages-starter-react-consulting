1a0abbdd172f275449ee308cc5d47bcda8e5b823
Aparna Ganesh <aganesh@yext.com>
2022-08-04 10:42:50 -0400

    J=CR-2656 fix(Hero): hours styling & null-checking

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Image, HoursStatus } from "@yext/sites-react-components";
 import {Address, Hours, CTA, Image as ImageType} from "@yext/types";
 import "src/styles/Hero.css";
+import ellipse from "src/assets/images/ellipse.svg";
 
 type HeroProps = {
     name: string;
@@ -25,9 +26,11 @@ const Hero = (props: HeroProps) => {
                     <div className="Hero-geo Heading Heading--lead">
                         {props.address.line1}
                     </div>
-                    <div className="Hero-hours">
-                        <HoursStatus hours={props.hours} />
-                    </div>
+                    {props.hours && (
+                        <div className="Hero-hours">
+                            <HoursStatus hours={props.hours} separatorTemplate={() => <img className="Hero-hourSeparator" src={ellipse} />} dayOfWeekTemplate={()=> null } /> 
+                        </div>
+                    )}
                     {/* TODO(aganesh) : use Reviews component when available */}
                     {props.rating && (
                         <div className="Hero-reviews">
@@ -35,14 +38,18 @@ const Hero = (props: HeroProps) => {
                             <span className="Hero-numReviews">({props.numReviews} reviews)</span>
                         </div>
                     )}
-                    <div className="Hero-linkWrapper">
-                        {props.cta1 && (<Link className="Hero-cta Button Button--primary" cta={props.cta1} />)}
-                        {props.cta2 && (<Link className="Hero-cta Button Button--secondary" cta={props.cta2} />)}
+                    {(props.cta1 || props.cta2) && (
+                        <div className="Hero-linkWrapper">
+                            {props.cta1 && (<Link className="Hero-cta Button Button--primary" cta={props.cta1} />)}
+                            {props.cta2 && (<Link className="Hero-cta Button Button--secondary" cta={props.cta2} />)}
+                        </div>
+                    )}
+                </div>
+                {props.background && (
+                    <div className="Hero-imageWrapper">
+                        <Image className="Hero-image" imageField={props.background}/>
                     </div>
-                </div>
-                <div className="Hero-imageWrapper">
-                    {props.background && (<Image className="Hero-image" imageField={props.background}/>)}
-                </div>
+                )}
             </div>
         </div>
     )
