449b5f0c053b2c6366e5e655d1b6bd96359a4b61
Tristan Timblin <ttimblin@yext.com>
2022-08-04 09:09:42 -0400

    downgrade plop to stable version

--- plopfile.js
+++
@@ -1,9 +0,0 @@
-const config = (plop) => {
-  plop.load("./plopfiles/component.js");
-  plop.load("@yext/sites-react-starters");
-
-  // console.log(plop);
-  // console.log(plop.getGeneratorList());
-};
-
-export default config;
