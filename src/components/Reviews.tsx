efa838b321b66112c4d381b8b20793ed996555e9
Andy Luu <andyluu324@gmail.com>
2022-08-16 23:59:27 -0400

    feat(reviews): completed most of the reviews and testimonials components with tailwindcss
    J=CR-2655
    TEST=manual

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,122 +1,263 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import "src/styles/Reviews.css";
 import { BsStarHalf, BsStarFill, BsStar } from "react-icons/bs";
-
 interface ReviewsProps {
 
 }
 
-function currentDate() {
-  const date = new Date();
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  return months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
-}
-
-function generateOverallStars(rating: number, numReviews: number) {
-  const currentRating = rating;
-  const maxRating = 5.0;
-  var stars = [<div className="Reviews-overallRatingNumber">{currentRating}</div>];
-
-  if(currentRating < maxRating) {
-
-    var i = 0;
-    while(i < Math.floor(currentRating)){
-      stars.push(<BsStarFill className="Reviews-overallStar" />);
-      i++;
-    }
-    if(currentRating%1 != 0) {
-      stars.push(<BsStarHalf className="Reviews-overallStar" />);
-      i++;
-    }
-    while(i < maxRating){
-      stars.push(<BsStar className="Reviews-overallStar" />);
-      i++;
-    }
-
-    stars.push(<div className="Reviews-overallRatingCount">({numReviews} reviews)</div>);
-  }
-  else {
-    stars = [<div className="Reviews-overallRatingNumber">Invalid Rating</div>];
-  }
-
-  return stars;
-}
-
-function generateCurrentReviewStars(rating: number, numReviews: number) {
-  const currentRating = rating;
-  const maxRating = 5.0;
-  var stars = [<div className="Reviews-ratingNumber">{currentRating}</div>];
-
-  if(currentRating < maxRating) {
-
-    var i = 0;
-    while(i < Math.floor(currentRating)){
-      stars.push(<BsStarFill className="Reviews-ratingStar" />);
-      i++;
-    }
-    if(currentRating%1 != 0) {
-      stars.push(<BsStarHalf className="Reviews-ratingStar" />);
-      i++;
-    }
-    while(i < maxRating){
-      stars.push(<BsStar className="Reviews-ratingStar" />);
-      i++;
-    }
-  }
-  else {
-    stars = [<div className="Reviews-ratingNumber">Invalid Rating</div>];
-  }
-
-  return stars;
-}
-
 const Reviews = (props: ReviewsProps) => {
 
-  return (
-    <div className="Reviews">
-      <div className="Reviews-container container">
-        <div className="Reviews-titleAndRating">
-            <div className="Reviews-title">
-              Recent Reviews
-            </div>
-            {/* Ask Ben how to implement this */}
-            <div className="Reviews-overallRating flex">
-            {generateOverallStars(4.4, 21)}
-            </div>
-        </div>
-        <div className="Reviews-review">
-          <div className="Reviews-reviewerNameAndDate">
-            <div className="Reviews-reviewerName">
-                Name
-            </div>
-            <div className="Reviews-reviewDate">
-                {currentDate()}
-            </div>
-          </div>
-          <div className="Reviews-ratingAndResponse">
-            {/* Ask Ben how to implement this */}
-            <div className="Reviews-rating flex">
-            {generateCurrentReviewStars(4.4, 21)}
-            </div>
-            <div className="Reviews-reviewText">
-              Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque. Ut porta viverra est, ut dignissim elit elementum ut.
+  function currentDate(reviewDate: string) {
+
+    const date = new Date(reviewDate);
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
+
+  }
+  
+  function generateOverallStars(rating: number, numReviews: number) {
+
+    const currentRating = rating;
+    const maxRating = 5.0;
+    let stars = [<div className="Reviews-overallRatingNumber mr-3 font-bold text-base">{currentRating}</div>];
+  
+    if(currentRating <= maxRating) {
+  
+      let currentStarNum = 0;
+
+      while(currentStarNum < Math.floor(currentRating)){
+        stars.push(<BsStarFill className="Reviews-overallStarFilled my-auto" />);
+        currentStarNum++;
+      }
+
+      if(currentRating % 1 != 0) {
+        stars.push(<BsStarHalf className="Reviews-overallStarHalf my-auto" />);
+        currentStarNum++;
+      }
+
+      while(currentStarNum < maxRating){
+        stars.push(<BsStar className="Reviews-overallStar my-auto" />);
+        currentStarNum++;
+      }
+  
+      stars.push(<div className="Reviews-overallRatingCount ml-3 font-normal text-base">({numReviews} reviews)</div>);
+
+    }
+    else {
+
+      stars = [<div className="Reviews-overallRatingNumber">Invalid Rating</div>];
+
+    }
+  
+    return stars;
+
+  }
+  
+  function generateCurrentReviewStars(rating: number) {
+
+    const currentRating = rating;
+    const maxRating = 5.0;
+    let stars = [<div className="Reviews-ratingNumber mr-3 font-bold text-base">{currentRating}</div>];
+  
+    if(currentRating <= maxRating) {
+  
+      let currentStarNum = 0;
+
+      while(currentStarNum < Math.floor(currentRating)){
+        stars.push(<BsStarFill className="Reviews-ratingStarFilled my-auto" />);
+        currentStarNum++;
+      }
+
+      if(currentRating % 1 != 0) {
+        stars.push(<BsStarHalf className="Reviews-ratingStarHalf my-auto" />);
+        currentStarNum++;
+      }
+
+      while(currentStarNum < maxRating){
+        stars.push(<BsStar className="Reviews-ratingStar my-auto" />);
+        currentStarNum++;
+      }
+
+    }
+    else {
+
+      stars = [<div className="Reviews-ratingNumber">Invalid Rating</div>];
+
+    }
+  
+    return stars;
+
+  }
+  
+  function generateReviews(currentReviews: any)
+  {
+
+    const reviewsArrToReturn = [];
+  
+    for(var currentReviewNum = 0; currentReviewNum < currentReviews.length; currentReviewNum++){
+
+      if(currentReviews[currentReviewNum].comments != null) {
+
+        reviewsArrToReturn.push(
+          <div className="Reviews-review pt-6 flex flex-col lg:flex-row" key={currentReviews[currentReviewNum].$key.primary_key}>
+            <div className="Reviews-reviewerNameAndDate lg:w-1/2">
+              <div className="Reviews-reviewerName font-bold text-lg mb-2">
+                {currentReviews[currentReviewNum].authorName}
+              </div>
+              <div className="Reviews-reviewDate text-base mb-4">
+                {currentDate(currentReviews[currentReviewNum].reviewDate)}
+              </div>
             </div>
 
-            {/* Ask Ben how to make this not always appear */}
-            <div className="Reviews-businessResponse">
-              <div className="Reviews-businessName">
-                  Business Name
+            <div className="Reviews-ratingAndResponse">
+              <div className="Reviews-rating flex mb-2">
+                {generateCurrentReviewStars(currentReviews[currentReviewNum].rating)}
               </div>
-              <div className="Reviews-responseDate">
-                {currentDate()}
+              <div className="Reviews-reviewText text-base mb-6">
+                {currentReviews[currentReviewNum].content}
               </div>
-              <div className="Reviews-responseText">
-                Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor.
+
+              <div className="Reviews-businessResponse">
+                <div className="Reviews-businessName font-bold text-lg mb-2">
+                {currentReviews[currentReviewNum].entity.name}
+                </div>
+                <div className="Reviews-responseDate text-base mb-4 lg:mb-2">
+                  {currentDate(currentReviews[currentReviewNum].comments[0].commentDate)}
+                </div>
+                <div className="Reviews-responseText text-base mb-6">
+                  {currentReviews[currentReviewNum].comments[0].content}
+                </div>
               </div>
             </div>
           </div>
+
+        );
+
+      }
+      else {
+
+        reviewsArrToReturn.push(
+          <div className="Reviews-review pt-6 flex flex-col lg:flex-row" key={currentReviews[currentReviewNum].$key.primary_key}>
+            <div className="Reviews-reviewerNameAndDate lg:w-1/2">
+              <div className="Reviews-reviewerName font-bold text-lg mb-2">
+                {currentReviews[currentReviewNum].authorName}
+              </div>
+              <div className="Reviews-reviewDate text-base mb-4">
+                {currentDate(currentReviews[currentReviewNum].reviewDate)}
+              </div>
+            </div>
+            <div className="Reviews-ratingAndResponse">
+              <div className="Reviews-rating flex mb-2">
+                {generateCurrentReviewStars(currentReviews[currentReviewNum].rating)}
+              </div>
+              <div className="Reviews-reviewText text-base mb-6">
+                {currentReviews[currentReviewNum].content}
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+
+    return reviewsArrToReturn;
+
+  }
+
+  function PaginatedReviews() {
+    // We start with an empty list of items.
+    const [allPageTokens, setAllPageTokens] = useState(['']);
+    const [pageNumber, setPageNumber] = useState(0);
+    const [currentReviews, setCurrentReviews] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const limitOfReviews = 4;
+    const APICallURL = "https://streams-sbx.yext.com/v2/accounts/me/api/reviews?v=20220101&api_key=ae79e8eb05e10f03917d3f4836863ac7&entity.id=322312743840815756";
+  
+    useEffect(() => {
+
+      const APICallURLWithLimit = APICallURL.concat("", "&limit=", limitOfReviews.toString())
+      const resp: any = fetch(APICallURLWithLimit)
+        .then(resp => resp.json())
+        .then(resp => {
+          setPageCount(Math.ceil(resp.response.count/limitOfReviews))
+          allPageTokens.push(resp.response.nextPageToken);
+          setAllPageTokens(allPageTokens);
+          setCurrentReviews(resp.response.docs);
+        })
+
+    }, [])
+
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+
+      if(event.selected >= 1) {
+
+        let APICallURLWithLimitAndToken = APICallURL.concat("", "&limit=", limitOfReviews.toString(), "&pageToken=", allPageTokens[allPageTokens.length - 1])
+
+        const resp: any = fetch(APICallURLWithLimitAndToken)
+        .then(resp => resp.json())
+        .then(resp => {
+          allPageTokens.push(resp.response.nextPageToken);
+          setAllPageTokens(allPageTokens);
+        })
+
+      }
+
+      const APICallURLWithLimitAndToken = APICallURL.concat("", "&limit=", limitOfReviews.toString(), "&pageToken=", allPageTokens[event.selected])
+
+      const resp: any = fetch(APICallURLWithLimitAndToken)
+      .then(resp => resp.json())
+      .then(resp => {
+        setCurrentReviews(resp.response.docs);
+      })
+
+    }
+
+    return (
+      <>
+        <div className="Reviews-container">
+          {generateReviews(currentReviews)}
+        </div>
+        {<ReactPaginate
+        breakLabel=". . ."
+        nextLabel=""
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={2}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel=""
+        renderOnZeroPageCount={null}
+        containerClassName="Reviews-pageNumbersContainer flex mt-10 justify-center"
+        pageClassName="Reviews-pageNumber px-4 py-2"
+        pageLinkClassName="Reviews-pageNumberLink font-bold text-sm"
+        breakClassName="Reviews-break font-bold text-xl mt-auto mb-1.5"
+        breakLinkClassName="Reviews-breakLink"
+        activeClassName="Reviews-activePageNumber"
+        activeLinkClassName="Reviews-activePageNumberLink"
+        previousClassName="Reviews-previousButton flex"
+        nextClassName="Reviews-nextButton flex"
+        previousLinkClassName="Reviews-previousButtonLink my-auto mr-3 h-6 w-6"
+        nextLinkClassName="Reviews-nextButtonLink my-auto ml-3 h-6 w-6"
+        disabledClassName="Reviews-disabledButton"
+        disabledLinkClassName="Reviews-disabledButtonLink"
+      /> }
+      </>
+    );
+
+  }
+
+  return (
+    <div className="Reviews container px-4">
+      <div className="Reviews-titleAndRating text-center pb-8 mt-8">
+        <div className="Reviews-title mb-3 font-bold text-2xl">
+          Recent Reviews
+        </div>
+        {/* Ask Ben how to implement this */}
+        <div className="Reviews-overallRating flex justify-center">
+        {generateOverallStars(4.4, 21)}
         </div>
       </div>
+      {<PaginatedReviews />}
     </div>
   );
 };
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,10 +1,7 @@
-
-import { Link } from "@yext/sites-react-components";
-import { CTA } from "@yext/types";
 import React from "react";
 import "src/styles/Testimonials.css";
 
-interface ReviewsProps {
+interface TestimonialsProps {
 
 }
 
@@ -14,114 +11,54 @@ function currentDate() {
   return months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
 }
 
-const Testimonials = (props: ReviewsProps) => {
+const Testimonials = (props: TestimonialsProps) => {
+
+  const qnaQuestionArr = ['Are you an existing client? (Y/N)', 'Do you have a conflict of interest with the advisor? (Y/N)', 'Have you received any compensation for leaving the review? (Y/N)', 'If Yes, provide a description of the material terms of compensation provided.'];
+  const qnaAnswerArr = ['Yes.', 'No.', 'No.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'];
+
+  function generateQNAElements(qnaQuestionArr: string[], qnaAnswerArr: string[]) {
+
+    var qnaArrToReturn = [];
+
+    for(var currQNANum = 0; currQNANum < qnaQuestionArr.length; currQNANum++) {
+
+      qnaArrToReturn.push(
+      <div className="Testimonials-qna lg:pr-2">
+        <div className="Testimonials-qnaQuestion text-sm font-bold">
+          {qnaQuestionArr[currQNANum]}
+        </div>
+        <div className="Testimonials-qnaAnswer text-sm font-normal">
+          {qnaAnswerArr[currQNANum]}
+        </div>
+      </div>);
+    }
+
+    return qnaArrToReturn;
+  }
 
   return (
     <div className="Testimonials">
-      <div className="Testimonials-container container">
-        <div className="Testimonials-title">
+      <div className="Testimonials-container container px-4">
+        <div className="Testimonials-title text-2xl font-bold text-center my-8 lg:mt-16 lg:mb-12">
           Testimonials
         </div>
-        <div className="Testimonials-review">
-          <div className="Testimonials-reviewContainer">
-            <div className="Testimonials-reviewText">
+        <div className="Testimonials-review flex flex-col mb-6	lg:mb-8 lg:flex-row">
+          <div className="Testimonials-reviewContainer p-6 lg:p-8">
+            <div className="Testimonials-reviewText text-lg font-normal pb-6 mb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className="Testimonials-qnaContainer">
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  Are you an existing client? (Y/N)
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  Yes.
-                </div>
-              </div>
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  Do you have a conflict of interest with the advisor? (Y/N)
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  No.
-                </div>
-              </div>
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  Have you received any compensation for leaving the review? (Y/N)
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  No.
-                </div>
-              </div>
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  If Yes, provide a description of the material terms of compensation provided.
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-              </div>
+            <div className="Testimonials-qnaContainer flex flex-col lg:flex-row lg:flex-wrap">
+              {generateQNAElements(qnaQuestionArr, qnaAnswerArr)}
             </div>
           </div>
-          <div className="Testimonials-reviewerContainer">
-            <div className="Testimonials-reviewerName">
+          <div className="Testimonials-reviewerContainer p-6 lg:p-8">
+            <div className="Testimonials-reviewerName font-bold">
               Name
             </div>
-            <div className="Testimonials-reviewDate">
+            <div className="Testimonials-reviewDate text-sm font-normal mb-4">
               {currentDate()}
             </div>
-            <div className="Testimonials-verifiedClient">
-              verified client
-            </div>
-          </div>
-        </div>
-        <div className="Testimonials-review">
-          <div className="Testimonials-reviewContainer">
-            <div className="Testimonials-reviewText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-            <div className="Testimonials-qnaContainer">
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  Are you an existing client? (Y/N)
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  Yes.
-                </div>
-              </div>
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  Do you have a conflict of interest with the advisor? (Y/N)
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  No.
-                </div>
-              </div>
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  Have you received any compensation for leaving the review? (Y/N)
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  No.
-                </div>
-              </div>
-              <div className="Testimonials-qna">
-                <div className="Testimonials-qnaQuestion">
-                  If Yes, provide a description of the material terms of compensation provided.
-                </div>
-                <div className="Testimonials-qnaAnswer">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="Testimonials-reviewerContainer">
-            <div className="Testimonials-reviewerName">
-              Name
-            </div>
-            <div className="Testimonials-reviewDate">
-              {currentDate()}
-            </div>
-            <div className="Testimonials-verifiedClient">
+            <div className="Testimonials-verifiedClient text-lg font-bold">
               verified client
             </div>
           </div>
