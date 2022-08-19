356f402e0064c202384c6c61dc01d04aafdc225f
bhaines <bhaines@yext.com>
2022-08-19 09:15:22 -0400

    use getRuntime for ssr check

--- src/common/useBreakpoints.tsx
+++ src/common/useBreakpoints.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect, useLayoutEffect } from "react";
 import resolveConfig from 'tailwindcss/resolveConfig'
+import { getRuntime } from "@yext/pages/util"
 import { screens as defaultScreens } from "tailwindcss/defaultTheme"
 //@ts-ignore: figure out how to use Tailwind's provided config type for this
 import * as tailwindConfig from '../../tailwind.config.cjs';
@@ -10,10 +11,8 @@ const fullConfig = resolveConfig(tailwindConfig);
 // TODO: get rid of this type cast. The possible types for screens are very flexible, which makes this complicated
 const screens: KeyValuePair<string, string> = (fullConfig.theme?.screens || defaultScreens) as KeyValuePair<string, string>;
 
-// https://github.com/pmndrs/zustand/blob/833f57ed131e94f3ed48627d4cfbf09cb9c7df03/src/react.ts#L20-L23
-export const isSSR = typeof window === "undefined" || !globalThis.navigator
-export const isBrowser = !isSSR;
-export const useIsomorphicEffect = isBrowser ? useLayoutEffect : useEffect;
+const runtime = getRuntime();
+const useIsomorphicEffect = runtime.name === "node" ? useEffect : useLayoutEffect;
 
 export function useBreakpoint(
   // TODO: this should be `keyof typeof screens`, but because types don't exist for the imported file that breaks
@@ -25,7 +24,7 @@ export function useBreakpoint(
   const [match, setMatch] = useState(defaultValue);
 
   useIsomorphicEffect(() => {
-    if (!(isBrowser && "matchMedia" in window)) return undefined;
+    if (!(runtime.name === "browser" && "matchMedia" in window)) return undefined;
 
     const value = screens[breakpoint];
     const query = window.matchMedia(`(min-width: ${value})`);
