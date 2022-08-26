8b6bea422bb0a0cdf4a49786ae10fe44aa053cfa
bhaines <bhaines@yext.com>
2022-08-26 14:08:38 -0400

    remove duplicate plugin definition

--- tailwind.ts
+++ tailwind.ts
@@ -1,20 +1,5 @@
 import React from 'react';
-import plugin from 'tailwindcss/plugin';
 
 export type ComponentDefinitions = Record<string, React.CSSProperties | {'&:hover': React.CSSProperties}>;
 
 export type ButtonConfig = React.CSSProperties & {variants: Record<string, React.CSSProperties | {'&:hover': React.CSSProperties}>}
-
-export function styleguidePlugin() {
-  return plugin(({addComponents, theme}) => {
-    const components = Object.assign({}, {'.Button': theme('buttons') || {}});
-    delete components['.Button'].variants;
-
-    for (const [variant, styles] of Object.entries(theme('buttons.variants'))) {
-      components[`.Button--${variant}`] = styles;
-
-    }
-
-    addComponents(components);
-  })
-}
