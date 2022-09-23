7d6b505aeb3dff16aea78e6393620eff7995a695
Alex Liang <aliang@yext.com>
2022-09-23 15:23:04 -0400

    fix(): footer
    - Update to use tailwind classes instead of css

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "@yext/pages/components";
 import type { CTA } from "@yext/types";
-import "src/components/common/Footer.css";
 import {
   FaFacebook,
   FaInstagram,
@@ -25,30 +24,36 @@ const Footer = (props: FooterProps) => {
   const copyrightMessage = props.copyrightMessage;
 
   const socialLinks = 
-  [{link: props.facebook, label: <FaFacebook />}, 
-   {link: props.instagram, label: <FaInstagram />},
-   {link: props.youtube, label: <FaYoutube />},
-   {link: props.linkedIn, label:<FaLinkedinIn />},
-   {link: props.twitter, label:<FaTwitter />}].filter(link => link.link);
+  [{link: props.facebook, label: <FaFacebook className="w-5 h-5 mr-4" />}, 
+   {link: props.instagram, label: <FaInstagram className="w-5 h-5 mr-4" />},
+   {link: props.youtube, label: <FaYoutube className="w-5 h-5 mr-4" />},
+   {link: props.linkedIn, label:<FaLinkedinIn className="w-5 h-5 mr-4" />},
+   {link: props.twitter, label:<FaTwitter className="w-5 h-5 mr-4" />}].filter(link => link.link);
 
   const footerLinks = props.footerLinks || [];
 
   return (
-    <footer className="Footer container">
-      <div className="Footer-linksContainer flex flex-col">
-        {footerLinks.map((link, i) => (
-          <Link className={'Footer-link Link Link--primary'} key={i} cta={link} />
-        ))}
-      </div>
-      <div className="Footer-socialContainer flex">
-        {socialLinks.map((socialLink, i) => (
-            <Link className={'Footer-socialLink Link Link--primary'} key={i} href={socialLink.link}>
-              {socialLink.label}
-            </Link>
-          ))}
-      </div>
-      <div className="Footer-copyright">
-        {copyrightMessage}
+    <footer className="Footer py-8 sm:py-16">
+      <div className="container">
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <div className="flex flex-col md:flex-row items-center">
+            {footerLinks.map((link, i) => (
+              <Link className="Link Link--primary mb-4 md:mb-0 md:mr-4" key={i} cta={link} />
+            ))}
+          </div>
+
+          <div className="my-4 md:my-0 flex flex-row items-center justify-center md:justify-end">
+            {socialLinks.map((socialLink, i) => (
+              <Link className="Link Link--primary" key={i} href={socialLink.link}>
+                {socialLink.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div className="text-sm text-center md:text-left mt-4">
+          {copyrightMessage}
+        </div>
       </div>
     </footer>
   );
