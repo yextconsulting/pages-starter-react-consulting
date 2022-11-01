88e77020485181aabd6b7c6e1062e93c1a797839
Ben Haines <bhainesva@users.noreply.github.com>
2022-11-01 08:17:46 -0400

    fix: export urlWriteback function as main rather than default (#106)
    TEST=manual

--- functions/onUrlChange/urlWriteback.ts
+++ functions/onUrlChange/urlWriteback.ts
@@ -3,7 +3,7 @@ import { urlWritebackPlugin } from "https://deno.land/x/yextpages@plugins@1.0.0-
 const pageUrlCustomField = "c_pagesUrl"; // Need to update to api name of CF storing prod URL for entities
 const API_KEY = "<REPLACE-ME>";
 
-export default urlWritebackPlugin({
+export const main = urlWritebackPlugin({
   field: pageUrlCustomField,
   apiKey: API_KEY,
   environment: "prod",
