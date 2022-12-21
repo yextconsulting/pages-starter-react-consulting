73643da96b48619d021cf8f1c9b83142a0dccac0
Tri Pham <>
2022-12-21 12:04:33 -0500

    feat(analytics): Add useExposeEnableYAFunction.tsx

---
+++ src/common/useExposeEnableYAFunction.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useAnalytics } from "@yext/pages/components";
+import { getRuntime } from '@yext/pages/util';
+
+declare global {
+  interface Window {
+    yextAnalyticsEnabled: boolean;
+    enableYextAnalytics: () => void;
+  }
+}
+
+// This provides the window.enableYextAnalytics function so that clients can enable Yext analytics
+// after gaining user consent.
+// This component must be a descendant of an AnalyticsProvider.
+export function useExposeEnableYAFunction() {
+  const analytics = useAnalytics();
+  if (!analytics) {
+    throw new Error("Attempted to enable Analytics outside of AnalyticsProvider")
+  }
+
+  if (!getRuntime().isServerSide) {
+    window.enableYextAnalytics = () => {
+      window.yextAnalyticsEnabled = true;
+      analytics.optIn();
+    };
+
+    // There's a fallback for window.enableYextAnalytics that sets window.yextAnalyticsEnabled
+    // to true in case a third-party script calls it before this code runs.
+    // If that has happened, call the real function now.
+    if (window.yextAnalyticsEnabled) {
+      window.enableYextAnalytics();
+    }
+  }
+}
