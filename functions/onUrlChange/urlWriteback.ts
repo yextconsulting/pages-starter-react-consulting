097b38b4dea7921ad8c9ac517323fd38480d88b2
jrhoads <jrhoads@yext.com>
2023-06-29 16:34:06 -0400

    fix(custom): fix typo in API name for c_pagesURL

--- functions/onUrlChange/urlWriteback.ts
+++ functions/onUrlChange/urlWriteback.ts
@@ -1,6 +1,6 @@
 import { urlWritebackPlugin } from "https://deno.land/x/yextpages@plugins@1.0.0-beta.3/mod.ts";
 
-const pageUrlCustomField = "c_pagesUrl"; // Need to update to api name of CF storing prod URL for entities
+const pageUrlCustomField = "c_pagesURL"; // Need to update to api name of CF storing prod URL for entities
 const API_KEY = "<REPLACE-ME>";
 
 export const main = urlWritebackPlugin({
