6d9199339f2d2015894dac658f21bab279431eee
Stephanie Severance <sseverance@yext.com>
2025-08-29 12:30:03 -0400

    feat(ErrorBoundaryWithAnalytics): add Sentry alerting
    - Sentry alerting was never implemented in the pages starter causing errors to go unnoticed. This along with alerting infra updates will make these errors known on future pages imps.
    
    J=none
    TEST=manual

--- src/components/common/ErrorBoundaryWithAnalytics.tsx
+++ src/components/common/ErrorBoundaryWithAnalytics.tsx
@@ -1,20 +1,25 @@
 import { AnalyticsScopeProvider } from "@yext/pages-components";
 import { ReactNode } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import * as Sentry from "@sentry/browser";
 
 type ErrorBoundaryWithAnalyticsProps = {
   name: string;
   children: ReactNode;
 };
 
+Sentry.init({
+  dsn: "https://c5880be0d3351c04bfdcee4623fa68c8@o4509159123058688.ingest.us.sentry.io/4509447365132289",
+});
+
 /**
  * Setup an error boundary around a child component to prevent the entire React app from
  * unmounting on an error. Also bundle the AnalyticsScopeProvider for convenience.
  */
 const ErrorBoundaryWithAnalytics = (props: ErrorBoundaryWithAnalyticsProps) => {
-  const handleError = () => {
-    // Setup any sentry reporting here.
+  const handleError = (err: Error) => {
     console.error(`Error occured in "${props.name}" scope.`);
+    Sentry.captureException(err);
   };
 
   return (
